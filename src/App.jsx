b89c7b6bb49907a0c45196fa3fe51e5ed009592c
Luhana <luhana410@gmail.com>
2025-10-16 14:02:09 -0500

    arreglo del app y main

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-<<<<<<< HEAD
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Somos from "./pages/Somos";
-import Mision from "./pages/Mision";
-import Contacto from "./pages/Contacto";
-
-export default function App() {
-  return (
-    <>
-      <Header />
-      <main style={{ padding: "20px" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/somos" element={<Somos />} />
-          <Route path="/mision" element={<Mision />} />
-          <Route path="/contacto" element={<Contacto />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
-}
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -56,8 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
->>>>>>> 7ea0f6109b4c5f38c77231c7a94fb426e164c13d
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
->>>>>>> 7ea0f6109b4c5f38c77231c7a94fb426e164c13d
+  </StrictMode>
+);
