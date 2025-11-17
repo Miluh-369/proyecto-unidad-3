3a76475cc6f6dec0c416a8b768d95d6b3ae305ea
Luhana <luhana410@gmail.com>
2025-11-17 18:22:12 -0500

    import fr

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import { useState } from 'react'
+
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, 
+    signInWithPopup, signOut} from 'firebase/auth'
+
+import { auth,Gprovider } from '../firebase/firebase';
+
+
 const Auth = () => {
   return (
     <div>Auth</div>
