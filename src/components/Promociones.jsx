550fb619ecf929fae59f54a665e1c3c994dae191
Luhana <luhana410@gmail.com>
2025-12-14 19:45:56 -0500

    promos

---
+++ src/components/Promociones.jsx
@@ -0,0 +1,14 @@
+import promocion1 from "../assets/promocion1.jpg";
+import promocion2 from "../assets/promocion2.jpg";
+import "../assets/css/Promociones.css"; 
+
+const Promociones = () => {
+  return (
+    <div>
+      <h1>Promociones</h1>
+      <p>Aquí van las promos ✨</p>
+    </div>
+  );
+};
+
+export default Promociones;
